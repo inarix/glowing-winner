c958654b760f6ab173de3e412765b40bc2be24c8
saisona <alexandre.saison.pro@gmail.com>
2021-03-18 15:50:27 +0100

    Update : with cat

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,12 @@
-#!/bin/sh
 # File              : entrypoint.sh
 # Author            : Alexandre Saison <alexandre.saison@inarix.com>
 # Date              : 18.03.2021
-# Last Modified Date: 18.03.2021
+# Last Modified Date: 19.03.2021
 # Last Modified By  : Alexandre Saison <alexandre.saison@inarix.com>
 set -e
-$return_value=$(yarn run test)
-echo "values=$return_value"
-echo "::set-output name=coverage::$return_value"
+echo "SI TU VOIS CA C'EST QUE C'EST BON !!!"
+yarn run test > debug.log
+echo "Debug.log =>\n"
+cat debug.log
+$return_value=`cat debug.log`
+echo "::set-output name=coverage::'$return_value'"
