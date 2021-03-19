56a55b324a9df56ae11f0e3a64747ec367f5d8ab
saisona <alexandre.saison.pro@gmail.com>
2021-03-19 10:35:05 +0100

    Update : adding yarn install before yarn test

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 # Last Modified Date: 19.03.2021
 # Last Modified By  : Alexandre Saison <alexandre.saison@inarix.com>
 set -e
-echo "SI TU VOIS CA C'EST QUE C'EST BON !!!"
+yarn install
 yarn run test > debug.log
 echo "Debug.log =>\n"
 cat debug.log
