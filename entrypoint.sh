715b72914b8a45cb75b0fd9949ff59a4df65ffe7
saisona <alexandre.saison.pro@gmail.com>
2021-03-19 14:42:26 +0100

    Update : adding double quote :
    Source : https://github.community/t/set-output-truncates-multiline-strings/16852/5

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@
 # Last Modified By  : Alexandre Saison <alexandre.saison@inarix.com>
 set -e
 yarn run test &> debug.log
-DEBUG=$(cat debug.log |  tail -n +12)
+DEBUG="$(cat debug.log |  tail -n +12)"
 echo "DEBUG='$DEBUG'"
 echo "::set-output name=coverage::'$DEBUG'"
 
