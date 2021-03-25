76ca90b17e9ff002eee72a768e59b9e89830a17a
saisona <alexandre.saison.pro@gmail.com>
2021-03-25 10:45:43 +0100

    Update: remove substitution

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,11 @@
 # File              : entrypoint.sh
 # Author            : Alexandre Saison <alexandre.saison@inarix.com>
 # Date              : 18.03.2021
-# Last Modified Date: 23.03.2021
+# Last Modified Date: 25.03.2021
 # Last Modified By  : Alexandre Saison <alexandre.saison@inarix.com>
-yarn run test > debug.log
-DEBUG="$(cat debug.log | grep -n -C2 -e 'Stmts')"
+yarn run test &> debug.log
+DEBUG=$(cat debug.log | grep -n -C2 -e 'Stmts')
 rm debug.log
 echo "DEBUG='$DEBUG'"
-DEBUG="${DEBUG//$'\n'/'%0A'}"
-DEBUG="${DEBUG//$'\r'/'%0D'}"
 echo "::set-output name=coverage::'$DEBUG'"
 
