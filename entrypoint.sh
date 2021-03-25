3232468eb0451e1149cecd40f0051c517b0abcc8
saisona <alexandre.saison.pro@gmail.com>
2021-03-25 10:54:07 +0100

    Update: get back ""

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@
 # Last Modified Date: 25.03.2021
 # Last Modified By  : Alexandre Saison <alexandre.saison@inarix.com>
 yarn run test &> debug.log
-DEBUG=$(cat debug.log | grep -n -C2 -e 'Stmts')
+DEBUG="$(cat debug.log | grep -n -C2 -e 'Stmts')"
 rm debug.log
 echo "DEBUG='$DEBUG'"
 echo "::set-output name=coverage::'$DEBUG'"
