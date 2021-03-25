ac240ee20adeab8865bfcb3d6c3ea19d1180e1c5
saisona <alexandre.saison.pro@gmail.com>
2021-03-25 15:28:37 +0100

    Update: go to 12.0.0-slim
     -> use #!/bin/bash

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # File              : entrypoint.sh
 # Author            : Alexandre Saison <alexandre.saison@inarix.com>
 # Date              : 25.03.2021
@@ -7,7 +7,7 @@
 yarn run test > debug.log
 DEBUG="$(cat debug.log | tail -n +6)"
 rm debug.log
-echo "DEBUG='$DEBUG'"
+echo "$DEBUG"
 DEBUG="${DEBUG//$'\n'/'%0A'}"
 echo "::set-output name=coverage::'$DEBUG'"
 
