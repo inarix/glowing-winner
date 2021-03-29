60fb371366b1e0187fd96f31f40c767985c872af
saisona <alexandre.saison.pro@gmail.com>
2021-03-29 11:31:52 +0200

    Update: remove debug log

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,11 @@
 # File              : entrypoint.sh
 # Author            : Alexandre Saison <alexandre.saison@inarix.com>
 # Date              : 25.03.2021
-# Last Modified Date: 25.03.2021
+# Last Modified Date: 29.03.2021
 # Last Modified By  : Alexandre Saison <alexandre.saison@inarix.com>
 yarn run test > debug.log
 DEBUG="$(cat debug.log | tail -n +6)"
 rm debug.log
-echo "$DEBUG"
 DEBUG="${DEBUG//$'\n'/'%0A'}"
 echo "::set-output name=coverage::'$DEBUG'"
 
